695f66847df84f8fa0af3b81cb52290f4c3e5e80
sandhya225 <77093328+sandhya225@users.noreply.github.com>
2022-10-24 09:46:03 +0530

    24-10-2022
    Local Changes

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import ClientConsole from './ClientConsole'
+import './App.css';
+<script
+  src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+></script>;
+function App() {
+  return (
+    <div className="App">
+    <ClientConsole />
+    </div>
+  );
+}
+
+export default App;
